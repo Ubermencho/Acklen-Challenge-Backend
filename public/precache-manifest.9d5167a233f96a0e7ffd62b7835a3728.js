8c225b566810ef8f16a2b1437898863caea45ce8
Oscar Flores <oflores482@gmail.com>
2020-07-25 22:14:44 -0600

    Heroku deploy V1

---
+++ public/precache-manifest.9d5167a233f96a0e7ffd62b7835a3728.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0886fed6d4457ac1d88ba166c01cba6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40050f3cc8fb74d98cd7",
+    "url": "/static/css/2.cbeaa07e.chunk.css"
+  },
+  {
+    "revision": "a0d4ff8e2da9fb41e625",
+    "url": "/static/css/main.ed10bdfe.chunk.css"
+  },
+  {
+    "revision": "40050f3cc8fb74d98cd7",
+    "url": "/static/js/2.f734b13b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f734b13b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d4ff8e2da9fb41e625",
+    "url": "/static/js/main.1fa74cb8.chunk.js"
+  },
+  {
+    "revision": "3d78e06a00c17cd4a94f",
+    "url": "/static/js/runtime-main.20cf3dc7.js"
+  }
+]);
